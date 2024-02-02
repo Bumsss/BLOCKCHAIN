7a0589e00185ee531ccabea5a9159632004a6090
allenbumanlag <98503749+Bumsss@users.noreply.github.com>
2024-02-02 14:32:04 +0800

    Add files via upload

---
+++ prelimexam/biblequiz.jsx
@@ -0,0 +1,13 @@
+function Correct(){
+    return(
+        <div>
+            <h1> The Bible Quiz </h1>
+            <h2> The Bible consists of the Old and New Testaments.
+                 Moses led the Istralites out of Egypt.
+                 The sermon on the Mount is found in the Book of Luke.
+            </h2>
+            
+
+        </div>
+    );
+}
